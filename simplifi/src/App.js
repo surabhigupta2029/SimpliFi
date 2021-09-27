842e5f1d745a4c2e24350a23ad097187a844e164
Fernando Gonzalez <fernando.gonz9732@gmail.com>
2021-09-26 20:44:48 -0700

    Home Page
    Rough draft of home page

--- simplifi/src/App.js
+++ simplifi/src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Footer from "./components/Footer"
 
 function App() {
   return (  
     <>
     <Header />
+    <Hero />
+    <Footer />
     </>
   );
 }
---
+++ simplifi/src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import classes from './Hero.module.scss';
+
+function Hero() {
+    return (
+        <hero>
+            <video src="/videos/video.mp4" autoPlay mute loop/>
+        <div className={classes.hero__intro}>
+            <h1>Welcome to Simplifi!</h1>
+            <p>Content for the website descritpion will go here!</p>
+        </div>
+        
+        <div className={classes.hero__content}>
+           <div className={classes.hero__content__circle}></div>
+           <div className={classes.hero__content__circle}></div>
+           <div className={classes.hero__content__circle}></div>
+        </div>
+        </hero>
+    )
+}
+
+export default Hero
